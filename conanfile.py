56635cf8e6e2c960ef611303ee0b35b371a97a68
Steve Robinson <ssrobins@gmail.com>
2019-02-10 22:31:24 -0800

    Add CoreBluetooth framework, which is required after recent source files were added to SDL2

--- conanfile.py
+++ conanfile.py
@@ -71,7 +71,7 @@ class Conan(ConanFile):
             for framework in frameworks:
                 self.cpp_info.exelinkflags.append("-framework %s" % framework)
         elif self.settings.os == "iOS":
-            frameworks = ["AVFoundation", "CoreGraphics", "CoreMotion", "Foundation", "GameController", "Metal", "OpenGLES", "QuartzCore", "UIKit", "CoreVideo", "IOKit", "CoreAudio", "AudioToolbox"]
+            frameworks = ["AVFoundation", "CoreBluetooth", "CoreGraphics", "CoreMotion", "Foundation", "GameController", "Metal", "OpenGLES", "QuartzCore", "UIKit", "CoreVideo", "IOKit", "CoreAudio", "AudioToolbox"]
             for framework in frameworks:
                 self.cpp_info.exelinkflags.append("-framework %s" % framework)
         elif self.settings.os == "Android":
