310fa2bfd80628d15bc5405281813e6e28557ba5
Steve Robinson <ssrobins@gmail.com>
2020-03-29 13:14:07 -0700

    Add CoreBluetooth framework to iOS to fix linker error in sdl2-example

--- conanfile.py
+++ conanfile.py
@@ -73,7 +73,7 @@ class Conan(ConanFile):
             self.cpp_info.debug.libs.extend(system_libs)
             self.cpp_info.release.libs.extend(system_libs)
         elif self.settings.os == "iOS":
-            frameworks = ["AudioToolbox", "AVFoundation", "CoreAudio", "CoreGraphics", "CoreMotion", "CoreVideo", "Foundation", "GameController", "IOKit", "Metal", "OpenGLES", "QuartzCore", "UIKit"]
+            frameworks = ["AudioToolbox", "AVFoundation", "CoreAudio", "CoreBluetooth", "CoreGraphics", "CoreMotion", "CoreVideo", "Foundation", "GameController", "IOKit", "Metal", "OpenGLES", "QuartzCore", "UIKit"]
             for framework in frameworks:
                 self.cpp_info.exelinkflags.append(f"-framework {framework}")
         elif self.settings.os == "Android":
