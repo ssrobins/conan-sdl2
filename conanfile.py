d404f67bfc8d976944cb7b20d41c5dcc5b36c1de
Steve Robinson <ssrobins@gmail.com>
2022-05-21 11:58:56 -0700

    Revert "Fix framework definition"
    This reverts commit 53880d9f0afd1b7db333290dd6d520d936976672.

--- conanfile.py
+++ conanfile.py
@@ -73,7 +73,7 @@ class Conan(ConanFile):
             self.cpp_info.release.libs.extend(system_libs)
         elif self.settings.os == "Macos":
             self.cpp_info.libs.append("iconv")
-            self.cpp_info.frameworks = [
+            self.cpp_info.frameworks.extend([
                 "AudioToolbox",
                 "Carbon",
                 "Cocoa",
@@ -83,14 +83,14 @@ class Conan(ConanFile):
                 "ForceFeedback",
                 "GameController",
                 "IOKit",
-                "Metal"]
+                "Metal"])
         elif self.settings.os == "Android":
             system_libs = ["android", "GLESv1_CM", "GLESv2", "log", "OpenSLES"]
             self.cpp_info.debug.libs.extend(system_libs)
             self.cpp_info.release.libs.extend(system_libs)
         elif self.settings.os == "iOS":
             self.cpp_info.libs.append("iconv")
-            self.cpp_info.frameworks = [
+            self.cpp_info.frameworks.extend([
                 "AudioToolbox",
                 "AVFoundation",
                 "CoreAudio",
@@ -105,6 +105,6 @@ class Conan(ConanFile):
                 "Metal",
                 "OpenGLES",
                 "QuartzCore",
-                "UIKit"]
+                "UIKit"])
         elif self.settings.os == "Android":
             self.cpp_info.libs.extend(["android"])
